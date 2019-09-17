b4b9659f1e06614cf138857a6fd040c67ea7e26d
Rune K. Svendsen <runesvend@gmail.com>
2019-09-17 13:56:10 +0200

    Exe: reorganize to support multiple executables
    Assert: pretty-print assertion error
    create-tables-exe: fix logging error

--- src/CryptoDepth/OrderBook/Db/Run/Assert.hs
+++ src/CryptoDepth/OrderBook/Db/Run/Assert.hs
@@ -13,6 +13,7 @@ import           Database.Beam.Migrate.Simple
 import           Database.Beam.Migrate.Types                (collectChecks)
 import qualified Database.Beam.Postgres                     as Pg
 import qualified Database.Beam.Postgres.Migrate             as Pg
+import qualified Text.Pretty.Simple                         as Pretty
 
 
 -- | Assert that the database has been migrated to the current schema
@@ -20,16 +21,14 @@ assertSchema :: Pg.Connection -> IO ()
 assertSchema conn = do
     actualDbState   <- Pg.getDbConstraints conn
     expectedDbState <- collectChecks <$> mkCheckedDb CT.dbCreate
-    unless (all (`elem` actualDbState) expectedDbState) $
-        error $ unlines
-            [ "Unsupported DB schema."
-            , "Missing:"
-            , show $ expectedDbState \\ actualDbState
-            , "Found:"
-            , show actualDbState
-            , "Expected:"
-            , show expectedDbState
-            ]
+    unless (all (`elem` actualDbState) expectedDbState) $ do
+        putStrLn "Missing:"
+        Pretty.pPrint $ expectedDbState \\ actualDbState
+        putStrLn "Found:"
+        Pretty.pPrint actualDbState
+        putStrLn "Expected:"
+        Pretty.pPrint expectedDbState
+        error "Unsupported DB schema. See details above."
   where
     mkCheckedDb migration =
         runMigrationSteps 0 Nothing migration (\_ _ step -> pure (runMigrationSilenced step))

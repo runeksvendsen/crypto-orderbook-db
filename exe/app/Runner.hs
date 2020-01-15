71bb7179b0f764ca5c9c7361a797809ab0112006
Rune K. Svendsen <runesvend@gmail.com>
2020-01-15 10:59:36 +0100

    foreverWithPauseRange: log timestamp instead of TimeUnit
    Given an interval of e.g. minPause="1 day" maxPause="2 days" the old way
    of logging would always log "1 day", even if the actual duration is e.g.
    1.5 days.
    
    Fix this by instead logging the  timestamp at which the pause ends.

--- exe/app/Runner.hs
+++ exe/app/Runner.hs
@@ -21,6 +21,7 @@ import           Control.Exception      (catch, SomeException)
 import qualified Data.Time.Units        as TU
 import qualified System.Random          as Random
 import qualified Control.Logging        as Log
+import qualified Data.Time.Clock        as C
 
 
 -- | Do something forever while pausing a random interval between.
@@ -37,12 +38,21 @@ foreverWithPauseRange
 foreverWithPauseRange minPause maxPause action =
     forever $ do
         _ <- action
-        pauseMicroseconds <- Random.randomRIO
+        pauseDuration <- TU.fromMicroseconds <$> Random.randomRIO
             (TU.toMicroseconds minPause, TU.toMicroseconds maxPause)
-        logInfoS "RUNNER" $ "Pausing for " ++ show (TU.fromMicroseconds pauseMicroseconds :: minPause)
-        threadDelay (fromIntegral pauseMicroseconds)
+        durationEnd <- durationEndTime pauseDuration
+        logInfoS "RUNNER" $ "Pausing until " ++ show durationEnd
+        threadDelay (fromIntegral $ TU.toMicroseconds pauseDuration)
+        logInfoS "RUNNER" "Pause over"
   where
     logInfoS = Log.loggingLogger Log.LevelInfo
+    addDuration :: C.UTCTime -> TU.Picosecond -> C.UTCTime
+    addDuration utcTime duration = C.addUTCTime
+        (fromRational $ (realToFrac (TU.toMicroseconds duration) :: Rational) / 1e6)
+        utcTime
+    durationEndTime duration = do
+        now <- C.getCurrentTime
+        return (addDuration now duration)
 
 -- | Run an IO action, and catch & log a thrown exception
 --    as an error.
